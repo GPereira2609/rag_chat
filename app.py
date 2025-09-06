7b2a5fc2dffc12d9e4a43b92a90cf242be77981b
Gabriel <gabriel.pereira@nees.ufal.br>
2025-09-06 12:02:23 -0300

    feat: aviso de ia

--- app.py
+++ app.py
@@ -15,11 +15,19 @@ def interface():
     if "qa_chain" not in st.session_state:
         with st.spinner("Conectando ao banco de dados e processando documentos..."):
             st.session_state.qa_chain = start_rag_agent()
-            st.success("BURRO está pronto para responder suas perguntas!")
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
     st.title("Bem-vindo ao BURRO - Bot Universitário de Respostas Rápidas e Otimizadas")
+    
+    st.markdown(
+    """
+    ⚠️ **Aviso:** As respostas apresentadas foram elaboradas com apoio de IA e baseadas nos materiais fornecidos. 
+    Revise sempre com seu professor ou em fontes de estudo confiáveis.
+    """,
+    unsafe_allow_html=True
+)
+
     st.divider()
 
     for message in st.session_state.messages:
